316d57daa4f4334bf08169622b0a294451b156ff
Onyebuci Matthew Mordi <mordio30@gmail.com>
2025-02-27 11:22:30 -0800

    completed assignment

--- create_schema.sql
+++ create_schema.sql
@@ -7,3 +7,28 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id       serial PRIMARY KEY,
+  line_1   varchar(255) NOT NULL,
+  city     varchar(255) NOT NULL,
+  state    varchar(50) NOT NULL,
+  zipcode  varchar(20) NOT NULL
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id      serial PRIMARY KEY,
+  name    varchar(255) NOT NULL,
+  credits integer NOT NULL
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id         serial PRIMARY KEY,
+  student_id integer REFERENCES students(id) ON DELETE CASCADE,
+  class_id   integer REFERENCES classes(id) ON DELETE CASCADE,
+  grade      varchar(2) -- this allows letter grades
+);
+
